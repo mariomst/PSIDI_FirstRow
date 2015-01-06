7c4aa849c81fca27ffdbd4662e66c76b88aee57c
Ruben Barros <ruben.23@live.com.pt>
2015-01-06 17:11:08 +0000

    App - order updated

--- App/app/scripts/controllers/user/order.js
+++ App/app/scripts/controllers/user/order.js
@@ -12,71 +12,85 @@ angular.module('iPhotoApp')
     $routeProvider
       .when('/user/:id/order/create', {templateUrl: 'views/user/order_form.html', controller: 'UserOrderCtrl' })
   })
-  .controller('UserOrderCtrl', function ($scope, $controller) {
+  .controller('UserOrderCtrl', function ($scope, $controller, $http, $location, Iphotoshare) {
 
     angular.extend(this, $controller('CommonFunctionsCtrl', {$scope: $scope}));
 
     $scope.order = {};
 
-    $scope.print_albums = [
-      {
-        "id": 1,
-        "theme": "Férias",
-        "title": "Melhores das férias",
-        "message": "I will get a Triforce, like I said",
-        "photos": [
-          {
-            "id": 1,
-            "albumId": 1,
-            "photo": "images/abc.jpg",
-            "date": 1418066023321
-          },
-          {
-            "id": 2,
-            "albumId": 1,
-            "photo": "images/abcd.jpg",
-            "date": 1418066023321
-          },
-          {
-            "id": 3,
-            "albumId": 1,
-            "photo": "images/abcde.jpg",
-            "date": 1418066023321
-          },
-          {
-            "id": 4,
-            "albumId": 1,
-            "photo": "images/abcdef.jpg",
-            "date": 1418066023321
-          },
-          {
-            "id": 5,
-            "albumId": 1,
-            "photo": "images/abcdefg.jpg",
-            "date": 1418066023321
-          }
-        ]
-      },
-      {
-        "id": 3,
-        "title": "cenas",
-        "photos": []
-      },
-      {
-        "id": 4,
-        "title": "trinta",
-        "photos": []
-      },
-      {
-        "id": 5,
-        "title": "bom album",
-        "photos": []
-      }
-    ];
+    //$scope.print_albums = [
+    //  {
+    //    "id": 1,
+    //    "theme": "Férias",
+    //    "title": "Melhores das férias",
+    //    "message": "I will get a Triforce, like I said",
+    //    "photos": [
+    //      {
+    //        "id": 1,
+    //        "albumId": 1,
+    //        "photo": "images/abc.jpg",
+    //        "date": 1418066023321
+    //      },
+    //      {
+    //        "id": 2,
+    //        "albumId": 1,
+    //        "photo": "images/abcd.jpg",
+    //        "date": 1418066023321
+    //      },
+    //      {
+    //        "id": 3,
+    //        "albumId": 1,
+    //        "photo": "images/abcde.jpg",
+    //        "date": 1418066023321
+    //      },
+    //      {
+    //        "id": 4,
+    //        "albumId": 1,
+    //        "photo": "images/abcdef.jpg",
+    //        "date": 1418066023321
+    //      },
+    //      {
+    //        "id": 5,
+    //        "albumId": 1,
+    //        "photo": "images/abcdefg.jpg",
+    //        "date": 1418066023321
+    //      }
+    //    ]
+    //  },
+    //  {
+    //    "id": 3,
+    //    "title": "cenas",
+    //    "photos": []
+    //  },
+    //  {
+    //    "id": 4,
+    //    "title": "trinta",
+    //    "photos": []
+    //  },
+    //  {
+    //    "id": 5,
+    //    "title": "bom album",
+    //    "photos": []
+    //  }
+    //];
 
     $scope.f = {
+      get: function(){
+        $scope.spinner = true;
+        $http.get(Iphotoshare.getUrl_Prefix() + '/users/' + $scope.user.userID + '/printAlbums')
+          .success(function(data){
+            $scope.spinner = false;
+            $scope.print_albums = data;
+          })
+          .error(function(error){
+            $scope.spinner = false;
+            console.log("error getting print_albums");
+          });
+      },
       submit: function(){
         console.log($scope.order);
       }
-    }
+    };
+
+    $scope.f.get();
   });
